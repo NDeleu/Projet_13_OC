e27025fe3605eb2bbf12410cc3d57d492569b1ab
Nicolas Deleu <n.deleu@hotmail.fr>
2023-08-01 21:32:06 +0200

    linting issues flake8 fixed

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -113,4 +113,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATICFILES_DIRS = [BASE_DIR / "static", ]
