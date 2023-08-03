e09979663a22e514a68fe0adcb855b0af257a668
Nicolas Deleu <n.deleu@hotmail.fr>
2023-08-03 20:42:40 +0200

    test yml25

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -155,7 +155,7 @@ if DEBUG:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [BASE_DIR / "static", ]
 else:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [BASE_DIR / "static", ]
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
