e802b349737da1f97c0f970c9c07bdbd0cdb9696
Nicolas Deleu <n.deleu@hotmail.fr>
2023-08-05 05:09:31 +0200

    continue sphinx final step

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,45 +1,32 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 import os
 import sys
 import django
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site/settings'
-django.setup()
-
 # Add the project root directory to the Python path
 sys.path.insert(0, os.path.abspath('../..'))
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath('../../oc_lettings_site/'))
-sys.path.insert(0, os.path.abspath('../../oc_lettings_site/tests/'))
-sys.path.insert(0, os.path.abspath('../../lettings/'))
-sys.path.insert(0, os.path.abspath('../../lettings/tests/'))
-sys.path.insert(0, os.path.abspath('../../profiles/'))
-sys.path.insert(0, os.path.abspath('../../profiles/tests/'))
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings'
+django.setup()
 
 project = 'Python OC Lettings FR'
 copyright = '2023, Nicolas Deleu'
 author = 'Nicolas Deleu'
 release = '1.0.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
+# Activez l'extension autodoc
 extensions = ['sphinx.ext.autodoc']
+
+# Incluez les modules à documenter (lettings, profiles, oc_lettings_site)
+autodoc_default_flags = [
+    'members', 'undoc-members', 'private-members', 'show-inheritance'
+]
+autodoc_modules = {
+    'lettings': 'lettings',
+    'profiles': 'profiles',
+    'oc_lettings_site': 'oc_lettings_site',
+}
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
